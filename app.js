59d6ab64101a694b15766ce47b0583484f6659f3
JustMendex <mehditobias@gmail.com>
2021-03-16 13:44:40 +0100

    [INITIAL SETUP]

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
-const xss = require('xss-clean');
-const multer = require('multer');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+const xss = require("xss-clean");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, path.join(__dirname, '/upload'));
+    cb(null, path.join(__dirname, "/upload"));
   },
   filename: function(req, file, cb) {
     cb(
       null,
       file.originalname
-        .split('.pdf')[0]
-        .split(' ')
-        .join('-') +
-        '-' +
+        .split(".pdf")[0]
+        .split(" ")
+        .join("-") +
+        "-" +
         Date.now() +
-        '.pdf'
+        ".pdf"
     );
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
 
-const globalErrorHandler = require('./controllers/errorController');
+const globalErrorHandler = require("./controllers/errorController");
 
-const everythingRouter = require('./routes/everythingRoutes');
+const everythingRouter = require("./routes/everythingRoutes");
 
 const app = express();
 
-app.enable('trust Proxy');
+app.enable("trust Proxy");
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
@@ -49,13 +49,16 @@ app.use(cors());
 // Set security HTTP headers
 app.use(helmet());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+//Disabled to run only on dev mode
+// app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '200kb' }));
-app.use(express.urlencoded({ extended: true, limit: '200kb' }));
+app.use(express.json({ limit: "200kb" }));
+app.use(express.urlencoded({ extended: true, limit: "200kb" }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -65,24 +68,24 @@ app.use(mongoSanitize());
 app.use(xss());
 
 app.use(
-  '/api/F893JG03VLKJ09C24V25',
+  "/api/F893JG03VLKJ09C24V25",
   upload.fields([
-    { name: 'lettreConsultation', maxCount: 1 },
-    { name: 'reglementConsultation', maxCount: 1 },
-    { name: 'offresConcurrents', maxCount: 1 },
-    { name: 'pvOuverture', maxCount: 1 },
-    { name: 'bonCommande', maxCount: 1 },
-    { name: 'pvReception', maxCount: 1 },
-    { name: 'facture', maxCount: 1 }
+    { name: "lettreConsultation", maxCount: 1 },
+    { name: "reglementConsultation", maxCount: 1 },
+    { name: "offresConcurrents", maxCount: 1 },
+    { name: "pvOuverture", maxCount: 1 },
+    { name: "bonCommande", maxCount: 1 },
+    { name: "pvReception", maxCount: 1 },
+    { name: "facture", maxCount: 1 },
   ]),
   everythingRouter
 );
 
-app.use('/pdf', express.static(__dirname + '/upload'));
+app.use("/pdf", express.static(__dirname + "/upload"));
 
-app.use('/', express.static(__dirname + '/dist'));
-app.all('*', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, './dist/') });
+app.use("/", express.static(__dirname + "/dist"));
+app.all("*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "./dist/") });
 });
 
 app.use(globalErrorHandler);
